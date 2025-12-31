1710149f4d950f1681f7a0e4d7b2697cac811927
recepcankececi <79525152+recepcankececi@users.noreply.github.com>
2025-12-31 17:15:24 +0300

    completed

---
+++ src/main/java/org/example/arge/CarSkeleton.java
@@ -0,0 +1,36 @@
+package org.example.arge;
+
+public class CarSkeleton {
+
+    private String name;
+    private String description;
+
+    public CarSkeleton() {
+    }
+
+    public CarSkeleton(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void startEngine() {
+        System.out.println(getClass().getSimpleName() + " engine starting");
+    }
+
+    public void drive() {
+        runEngine();
+        System.out.println(getClass().getSimpleName() + " driving");
+    }
+
+    protected void runEngine() {
+        System.out.println(getClass().getSimpleName() + " engine running");
+    }
+}
---
+++ src/main/java/org/example/arge/GasPoweredCar.java
@@ -0,0 +1,27 @@
+package org.example.arge;
+
+public class GasPoweredCar extends CarSkeleton {
+
+    private double avgKmPerLitre;
+    private int cylinders;
+
+    public GasPoweredCar(String name, String description,
+                         double avgKmPerLitre, int cylinders) {
+        super(name, description);
+        this.avgKmPerLitre = avgKmPerLitre;
+        this.cylinders = cylinders;
+    }
+
+    public double getAvgKmPerLitre() {
+        return avgKmPerLitre;
+    }
+
+    public int getCylinders() {
+        return cylinders;
+    }
+
+    @Override
+    public void startEngine() {
+        System.out.println("Gas engine starting with " + cylinders + " cylinders");
+    }
+}
